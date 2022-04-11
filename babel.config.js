4c33927d3db28ecc40839c55e467a92cacf8c1cf
zhiwei.zheng <zhiwei.zheng@cloudwise.com>
2022-04-11 17:28:39 +0800

    init 初始化环境

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,15 @@
-/**
- * @name: babel.config.js
- * @author: evil
- * @date: 2022-03-20 17:55
- * @description：babel.config.js
- * @update: 2022-03-20 17:55
- */
-const env = process.env.NODE_ENV
 module.exports = {
-  presets: [
-    ["@babel/preset-env", {
-      modules: false, // 开启tree-shaking
-      useBuiltIns: 'entry',
-      corejs: 3
-    }],
-    ["@babel/preset-react"],
-    ["@babel/preset-typescript"]
-  ],
-  plugins: [env === 'development' && require.resolve('react-refresh/babel')].filter(Boolean)
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                targets: {
+                    node: "current"
+                }
+            }
+        ],
+        [
+            "@babel/preset-typescript"
+        ]
+    ]
 }
