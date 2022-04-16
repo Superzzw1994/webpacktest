cda2395fe0a05d97e16800772f1af29afb0a5932
邪恶天才 <shzx1994529@gmail.com>
2022-04-16 12:01:36 +0800

    Feat 升级框架，增加eslint,prettier,husky,lint-staged

--- src/coffees/dto/modify-coffee.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCoffeeDto } from './create-coffee.dto';
-
-export class ModifyCoffeeDto extends PartialType(CreateCoffeeDto) {}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      }
-    }),
-  );
-  await app.listen(3000);
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
 }
 
-bootstrap();
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
